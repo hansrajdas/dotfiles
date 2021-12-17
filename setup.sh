853b48cc397ec34cac04deb43d254354f3e3d459
Hansraj Das <raj.das.136@gmail.com>
2021-12-18 02:31:31 +0530

    Remove common.vim and move all it's contents to vimrc file

--- setup.sh
+++ setup.sh
@@ -1,21 +1,30 @@
 #!/usr/local/bin/bash
 
-BASE=$(pwd)
+BASE=$PWD
+BACKUP_EXT=$(date +%s)
 
 # 1. Setup VIM
 mkdir -p ~/.vim/autoload
 
-curl --insecure -fLo ~/.vim/autoload/plug.vim https://raw.github.com/junegunn/vim-plug/master/plug.vim
+curl --insecure --create-dirs -fLo ~/.vim/autoload/plug.vim \
+    https://raw.github.com/junegunn/vim-plug/master/plug.vim
 
-mv -v ~/.vimrc ~/.vimrc.old 2> /dev/null
-ln -sf $BASE/vimrc ~/.vimrc
+mv -v ~/.vimrc ~/.vimrc.${BACKUP_EXT} 2> /dev/null
+ln -sf ${BASE}/vimrc ~/.vimrc
 
 vim +PlugInstall +qall
 
 # 2. bashrc
-curl --insecure -fLo ~/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-curl --insecure -fLo ~/completion.bash https://raw.githubusercontent.com/junegunn/fzf/master/shell/completion.bash
-curl --insecure -fLo ~/key-bindings.bash https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.bash
+curl --insecure -fLo ~/git-completion.bash \
+    https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl --insecure -fLo ~/completion.bash \
+    https://raw.githubusercontent.com/junegunn/fzf/master/shell/completion.bash
+curl --insecure -fLo ~/key-bindings.bash \
+    https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.bash
 
-mv -v ~/.bashrc ~/.bashrc.old 2> /dev/null
-ln -sf $BASE/bashrc ~/.bashrc
+mv -v ~/.bashrc ~/.bashrc.${BACKUP_EXT} 2> /dev/null
+ln -sf ${BASE}/bashrc ~/.bashrc
+
+# 3. tmux.conf
+mv -v ~/.tmux.conf ~/.tmux.conf.${BACKUP_EXT} 2> /dev/null
+ln -sf ${BASE}/tmux.conf ~/.tmux.conf
--- vimrc
+++ vimrc
@@ -1,2 +1,42 @@
-source $HOME/dotfiles/common.vim
+colorscheme default
+set ignorecase
+set number
+set autoindent
+set incsearch
+set tabstop=4
+set hlsearch
+set expandtab
+set colorcolumn=120
+set relativenumber
+set shiftwidth=4
+set cursorline
+
+" Enter the current millenium
+set nocompatible
+
+" Enable syntax and plugins
+syntax enable
+filetype plugin on
+
+" Search down into subfolders
+" Provides tab-completion for all file related tasks
+" Used by :find ...
+set path+=**
+
+" Skip *.pyc files while searching for file using find command
+set wildignore+=**/*.pyc
+
+" Display all matching files when we tab complete
+set wildmenu
+
 highlight ColorColumn ctermbg=red
+
+" Start plugins
+call plug#begin('~/.vim/plugged')
+Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
+Plug 'junegunn/fzf.vim'
+call plug#end()  " Initialize plugin system
+" End plugins
+
+" Mappings
+nnoremap <C-p> :Files<Cr>
